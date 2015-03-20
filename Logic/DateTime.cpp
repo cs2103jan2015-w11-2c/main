41dfb111c4e86214e5d0439e5107b89139f40496
shanshan <arashisuki33@gmail.com>
2015-03-20 20:48:47 +0800

    finish the change weekday to exact date method.

--- Logic/DateTime.cpp
+++ Logic/DateTime.cpp
@@ -57,101 +57,118 @@ public:
 		int wday = (year + year/4 - year/100 + year/400 + t[month - 1] + day) % 7;
 		return wday;
 	}
-	
+
 	// given which day in the week is the event, return the date
 	string getDateFromWeekDay(string weekDay){
-	map<int, string> weekDayList;
-	map<int, string> weekList;
-	string weekAndDay[1];
-    int spacePos = weekDay.find_first_of(" ");
-	
-	if(spacePos != string::npos){
-		weekAndDay[0] = weekDay.substr(0,spacePos);
-		weekAndDay[1] = weekDay.substr(spacePos);
-	
-	}else{
-		weekAndDay[0] = NULL;
-		weekAndDay[1] = weekDay;
-	}  
+		map<int, string> weekDayList;
+		map<int, string> weekList;
+		string weekAndDay[1];
+		int spacePos = weekDay.find_first_of(" ");
 
-	weekDayList[1] = "Monday";
-	weekDayList[2] = "Tuesday";
-	weekDayList[3] = "Wednesday";
-	weekDayList[4] = "Thursday";
-	weekDayList[5] = "Friday";
-	weekDayList[6] = "Saturday";
-	weekDayList[7] = "Sunday";
+		if(spacePos != string::npos){
+			weekAndDay[0] = weekDay.substr(0,spacePos);
+			weekAndDay[1] = weekDay.substr(spacePos);
+
+		}else{
+			weekAndDay[0] = NULL;
+			weekAndDay[1] = weekDay;
+		}  
+
+		weekDayList[1] = "Monday";
+		weekDayList[2] = "Tuesday";
+		weekDayList[3] = "Wednesday";
+		weekDayList[4] = "Thursday";
+		weekDayList[5] = "Friday";
+		weekDayList[6] = "Saturday";
+		weekDayList[7] = "Sunday";
 
 
 
-	if (weekAndDay[0] == "This" ) || (weekAndDay[0] == NULL){
-    int weekNo = 0;
-	}else if(weekAndDay[0] == "Next"){
-	int weekNo = 7;
-	}
-
-    string currentWeekDay = getCurrentWeekDay();
-	map<int, string>::iterator iter;
-	iter = weekDayList.begin();
-	while(iter->second != weekAndDay[1]){
-		iter++;
-	}
-	int weekDayNo = iter->first;
-
-	iter = weekDayList.begin();
-	while(iter->second != currentWeekDay){
-		iter++;
-	}
-	int currentWeekDayNo = iter->first;
-	int diffInDay = currentWeekDayNo - weekDayNo;
-	
-	get
-
-	
-	}
-
-	// returns the weekday name for the specified day, month and year
-	// Sunday, Monday, .... Saturday
-	string getWeekDay (int day, int month, int year) {
-		int wday = getIntWeekDay(day, month, year);
-		return WEEKDAY[wday];
-	}
-
-	string getMonth(const int mon) {
-		return MONTH[mon-1];
-	}
-
-	bool isLeapYear (int year) {
-		return (((year % 400) == 0) || (year % 4) == 0) && (((year % 100) != 0));
-	}
-
-	bool isValidDate(int day, int month, int year) {
-		if((day <= 0) || (month <= 0) || (day > 31) || (month > 12)) {
-			return false;
+		if ((weekAndDay[0] == "This" ) || (weekAndDay[0] == NULL )){
+			int weekNo = 0;
+		}else if(weekAndDay[0] == "Next"){
+			int weekNo = 7;
 		}
 
-		if((month == 4) || (month == 6) || (month == 9) || (month || 11)) {
-			if(day > 30) {
-				return false;
+		string currentWeekDay = getCurrentWeekDay();
+		map<int, string>::iterator iter;
+		iter = weekDayList.begin();
+		while(iter->second != weekAndDay[1]){
+			iter++;
+		}
+		int weekDayNo = iter->first;
+
+		iter = weekDayList.begin();
+		while(iter->second != currentWeekDay){
+			iter++;
+		}
+		int currentWeekDayNo = iter->first;
+		int diffInDay = weekDayNo - currentWeekDayNo;
+
+		int currentDay = getCurrentDay() + diffInDay;
+		int currentMonth = getCurrentMonth();
+
+		if((currentMonth == 4) || (currentMonth == 6) || (currentMonth == 9) || (currentMonth || 11)) {
+			if(currentDay > 30) {
+				currentDay = currentDay - 30;
+				currentMonth = currentMonth + 1;
+			}
+		}else if(currentMonth == 2){
+			if(currentDay > 28) {
+				currentDay = currentDay - 28;
+				currentMonth = currentMonth + 1;
+			}
+		}else {
+			if(currentDay > 31) {
+				currentDay = currentDay - 31;
+				currentMonth = currentMonth + 1;
 			}
 		}
 
-		if(month == 2) {
-			if(isLeapYear(year) && day <= 29) {
+	}
+
+			// returns the weekday name for the specified day, month and year
+			// Sunday, Monday, .... Saturday
+			string getWeekDay (int day, int month, int year) {
+				int wday = getIntWeekDay(day, month, year);
+				return WEEKDAY[wday];
+			}
+
+			string getMonth(const int mon) {
+				return MONTH[mon-1];
+			}
+
+			bool isLeapYear (int year) {
+				return (((year % 400) == 0) || (year % 4) == 0) && (((year % 100) != 0));
+			}
+
+			bool isValidDate(int day, int month, int year) {
+				if((day <= 0) || (month <= 0) || (day > 31) || (month > 12)) {
+					return false;
+				}
+
+				if((month == 4) || (month == 6) || (month == 9) || (month || 11)) {
+					if(day > 30) {
+						return false;
+					}
+				}
+
+				if(month == 2) {
+					if(isLeapYear(year) && day <= 29) {
+						return true;
+					} else return (day < 29);
+				}
+
+				if(year < MIN_YEAR || year > MAX_YEAR) {
+					return false;
+				}
+
 				return true;
-			} else return (day < 29);
-		}
+			}
 
-		if(year < MIN_YEAR || year > MAX_YEAR) {
-			return false;
-		}
-
-		return true;
-	}
-
-	bool DateTime::isValidTime(int hour, int minute) {
-		return (hour >= 0 && hour <= 24 && minute >= 0 && minute <= 60);
-	}
+			bool DateTime::isValidTime(int hour, int minute) {
+				return (hour >= 0 && hour <= 24 && minute >= 0 && minute <= 60);
+			}
 
 
-};
+		};
